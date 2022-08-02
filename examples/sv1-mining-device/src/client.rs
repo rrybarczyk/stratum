869fc169500f786bc74ed4117bae0b626a022b16
fi3 <email@email.org>
2022-08-02 13:26:15 +0200

    Udpate sv1 client to implement miner logic

--- examples/sv1-mining-device/src/client.rs
+++ examples/sv1-mining-device/src/client.rs
@@ -1,14 +1,23 @@
 use async_std::net::TcpStream;
 use std::convert::TryInto;
 
+use bitcoin::{
+    blockdata::block::BlockHeader,
+    hash_types::{BlockHash, TxMerkleNode},
+    hashes::{sha256d::Hash as DHash, Hash},
+    util::uint::Uint256,
+};
+
 use async_channel::{bounded, Receiver, Sender};
+
 use async_std::{
     io::BufReader,
     prelude::*,
-    sync::{Arc, Mutex},
     task,
 };
 use std::time;
+use std::sync::Arc;
+use roles_logic_sv2::utils::Mutex;
 
 const ADDR: &str = "127.0.0.1:34254";
 
@@ -26,20 +35,25 @@ pub struct Client {
     version_rolling_mask: Option<HexU32Be>,
     version_rolling_min_bit: Option<HexU32Be>,
     pub status: ClientStatus,
-    last_notify: Option<server_to_client::Notify>,
     sented_authorize_request: Vec<(String, String)>, // (id, user_name)
     authorized: Vec<String>,
     receiver_incoming: Receiver<String>,
     sender_outgoing: Sender<String>,
+    miner: Arc<Mutex<Miner>>,
 }
 
 impl Client {
-    pub async fn new(client_id: u32) -> Arc<Mutex<Self>> {
+    pub async fn new(client_id: u32) {
         let stream = std::sync::Arc::new(TcpStream::connect(ADDR).await.unwrap());
         let (reader, writer) = (stream.clone(), stream);
 
         let (sender_incoming, receiver_incoming) = bounded(10);
         let (sender_outgoing, receiver_outgoing) = bounded(10);
+        let (share_send, share_recv) = bounded(10);
+
+        let miner = Arc::new(Mutex::new(Miner::new(0)));
+        miner.safe_lock(|m| m.new_target(todo!())).unwrap();
+        let miner_cloned = miner.clone();
 
         task::spawn(async move {
             let mut messages = BufReader::new(&*reader).lines();
@@ -57,34 +71,52 @@ impl Client {
             }
         });
 
-        let client = Client {
+
+        let mut client = Client {
             client_id,
             extranonce1: "00000000".try_into().unwrap(),
             extranonce2_size: 2,
             version_rolling_mask: None,
             version_rolling_min_bit: None,
             status: ClientStatus::Init,
-            last_notify: None,
             sented_authorize_request: vec![],
             authorized: vec![],
             receiver_incoming,
             sender_outgoing,
+            miner,
         };
 
-        let client = Arc::new(Mutex::new(client));
+        client.send_configure().await;
 
-        let cloned = client.clone();
+        std::thread::spawn(move || loop {
+            if miner_cloned.safe_lock(|m| m.next_share()).unwrap().is_ok() {
+                let nonce = miner_cloned.safe_lock(|m| m.header.unwrap().nonce).unwrap();
+                let time = miner_cloned.safe_lock(|m| m.header.unwrap().time).unwrap();
+                let job_id = miner_cloned.safe_lock(|m| m.job_id).unwrap();
+                let version = miner_cloned.safe_lock(|m| m.version).unwrap();
+                share_send
+                    .try_send((nonce, job_id.unwrap(), version.unwrap(), time))
+                    .unwrap();
+            }
+            miner_cloned
+                .safe_lock(|m| m.header.as_mut().map(|h| h.nonce += 1))
+                .unwrap();
+        });
 
         task::spawn(async move {
+            let recv = share_recv.clone();
             loop {
-                if let Some(mut self_) = cloned.try_lock() {
-                    let incoming = self_.receiver_incoming.try_recv();
-                    self_.parse_message(incoming).await;
-                }
+                let (nonce, job_id, version, ntime) = recv.recv().await.unwrap();
+                // TODO create the message to send the share
+                // TODO use sender_outgoing send message and send the sahre
+                todo!()
             }
         });
 
-        client
+        loop {
+            let incoming = client.receiver_incoming.try_recv();
+            client.parse_message(incoming).await;
+        }
     }
 
     async fn parse_message(
@@ -94,73 +126,18 @@ impl Client {
         if let Ok(line) = incoming_message {
             println!("CIENT {} - message: {}", self.client_id, line);
             let message: json_rpc::Message = serde_json::from_str(&line).unwrap();
-            self.handle_message(message).unwrap();
+            match self.handle_message(message).unwrap() {
+                Some(m) =>  {
+                    self.send_message(m).await
+                },
+                None => (),
+            }
         };
     }
 
     async fn send_message(&mut self, msg: json_rpc::Message) {
         let msg = format!("{}\n", serde_json::to_string(&msg).unwrap());
         self.sender_outgoing.send(msg).await.unwrap();
-    }
-
-    pub async fn send_subscribe(&mut self) {
-        loop {
-            if let ClientStatus::Configured = self.status {
-                break;
-            }
-        }
-        let id = time::SystemTime::now()
-            .duration_since(time::SystemTime::UNIX_EPOCH)
-            .unwrap()
-            .as_nanos()
-            .to_string();
-        let subscribe = self.subscribe(id, None).unwrap();
-        self.send_message(subscribe).await;
-    }
-
-    //pub async fn restore_subscribe(&mut self) {
-    //    let id = time::SystemTime::now()
-    //        .duration_since(time::SystemTime::UNIX_EPOCH)
-    //        .unwrap()
-    //        .as_nanos()
-    //        .to_string();
-    //    let subscribe = self.subscribe(id, Some(self.extranonce1.clone())).unwrap();
-    //    self.send_message(subscribe).await;
-    //}
-
-    pub async fn send_authorize(&mut self) {
-        let id = time::SystemTime::now()
-            .duration_since(time::SystemTime::UNIX_EPOCH)
-            .unwrap()
-            .as_nanos()
-            .to_string();
-        let authorize = self
-            .authorize(id.clone(), "user".to_string(), "user".to_string())
-            .unwrap();
-        self.sented_authorize_request.push((id, "user".to_string()));
-        self.send_message(authorize).await;
-    }
-
-    pub async fn send_submit(&mut self) {
-        let id = time::SystemTime::now()
-            .duration_since(time::SystemTime::UNIX_EPOCH)
-            .unwrap()
-            .as_nanos()
-            .to_string();
-        let extranonce2 = "00".try_into().unwrap();
-        let nonce = 78;
-        let version_bits = None;
-        let submit = self
-            .submit(
-                id,
-                "user".to_string(),
-                extranonce2,
-                nonce,
-                nonce,
-                version_bits,
-            )
-            .unwrap();
-        self.send_message(submit).await;
     }
 
     pub async fn send_configure(&mut self) {
@@ -176,7 +153,8 @@ impl Client {
 
 impl IsClient for Client {
     fn handle_notify(&mut self, notify: server_to_client::Notify) -> Result<(), Error> {
-        self.last_notify = Some(notify);
+        let new_job: Job = notify.into();
+        self.miner.safe_lock(|m| m.new_header(new_job)).unwrap();
         Ok(())
     }
 
@@ -257,14 +235,91 @@ impl IsClient for Client {
     }
 
     fn last_notify(&self) -> Option<server_to_client::Notify> {
-        self.last_notify.clone()
+        None
     }
 
     fn handle_error_message(
         &mut self,
         message: v1::Message,
-    ) -> Result<Option<json_rpc::Response>, Error> {
+    ) -> Result<Option<json_rpc::Message>, Error> {
         println!("{:?}", message);
         Ok(None)
+    }
+}
+
+struct Job {
+    job_id: u32,
+    prev_hash: [u8;32],
+    nbits: u32,
+    version: u32,
+    merkle_root: [u8;32],
+}
+
+impl From<v1::methods::server_to_client::Notify> for Job {
+    fn from(_: v1::methods::server_to_client::Notify) -> Self {
+        todo!()
+    }
+}
+
+#[derive(Debug)]
+struct Miner {
+    header: Option<BlockHeader>,
+    target: Option<Uint256>,
+    job_id: Option<u32>,
+    version: Option<u32>,
+    handicap: u32,
+}
+
+impl Miner {
+    fn new(handicap: u32) -> Self {
+        Self {
+            target: None,
+            header: None,
+            job_id: None,
+            version: None,
+            handicap,
+        }
+    }
+
+    fn new_target(&mut self, target: Vec<u8>) {
+        self.target = Some(Uint256::from_be_bytes(target.try_into().unwrap()));
+    }
+
+    fn new_header(&mut self, new_job: Job) {
+        self.job_id = Some(new_job.job_id);
+        self.version = Some(new_job.version);
+        let prev_hash: [u8; 32] = new_job.prev_hash;
+        let prev_hash = DHash::from_inner(prev_hash);
+        let merkle_root: [u8; 32] = new_job.merkle_root.to_vec().try_into().unwrap();
+        let merkle_root = DHash::from_inner(merkle_root);
+        let header = BlockHeader {
+            version: new_job.version as i32,
+            prev_blockhash: BlockHash::from_hash(prev_hash),
+            merkle_root: TxMerkleNode::from_hash(merkle_root),
+            time: std::time::SystemTime::now()
+                .duration_since(
+                    std::time::SystemTime::UNIX_EPOCH - std::time::Duration::from_secs(60),
+                )
+                .unwrap()
+                .as_secs() as u32,
+            bits: new_job.nbits,
+            nonce: 0,
+        };
+        self.header = Some(header);
+    }
+    pub fn next_share(&mut self) -> Result<(), ()> {
+        let header = self.header.as_ref().ok_or(())?;
+        let mut hash = header.block_hash().as_hash().into_inner();
+        hash.reverse();
+        let hash = Uint256::from_be_bytes(hash);
+        if hash < *self.target.as_ref().ok_or(())? {
+            println!(
+                "Found share with nonce: {}, for target: {:?}",
+                header.nonce, self.target
+            );
+            Ok(())
+        } else {
+            Err(())
+        }
     }
 }
