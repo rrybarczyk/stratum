898fa2e556276ad21468a83825ab324f96f6ca0c
Rachel Rybarczyk <rj@rybar.tech>
2024-06-11 13:09:37 -0400

    Rm unuse Configuration + duration_from_toml + clean imports

--- roles/jd-server/src/lib/mod.rs
+++ roles/jd-server/src/lib/mod.rs
@@ -11,55 +11,8 @@ pub(crate) use error::{Error, Result};
 pub use error::{Error as JdsError, Result as JdsResult};
 
 use codec_sv2::{StandardEitherFrame, StandardSv2Frame};
-use key_utils::{Secp256k1PublicKey, Secp256k1SecretKey};
 use roles_logic_sv2::parsers::PoolMessages as JdsMessages;
-use serde::Deserialize;
-use std::time::Duration;
 
 pub type Message = JdsMessages<'static>;
 pub type StdFrame = StandardSv2Frame<Message>;
 pub type EitherFrame = StandardEitherFrame<Message>;
-
-#[derive(Debug, Deserialize, Clone)]
-pub struct Configuration {
-    pub listen_jd_address: String,
-    pub authority_public_key: Secp256k1PublicKey,
-    pub authority_secret_key: Secp256k1SecretKey,
-    pub cert_validity_sec: u64,
-    pub coinbase_outputs: Vec<config::CoinbaseOutput>,
-    pub core_rpc_url: String,
-    pub core_rpc_port: u16,
-    pub core_rpc_user: String,
-    pub core_rpc_pass: String,
-    #[serde(deserialize_with = "duration_from_toml")]
-    pub mempool_update_interval: Duration,
-}
-
-fn duration_from_toml<'de, D>(deserializer: D) -> std::result::Result<Duration, D::Error>
-where
-    D: serde::Deserializer<'de>,
-{
-    #[derive(Deserialize)]
-    struct Helper {
-        unit: String,
-        value: u64,
-    }
-
-    let helper = Helper::deserialize(deserializer)?;
-    match helper.unit.as_str() {
-        "seconds" => Ok(Duration::from_secs(helper.value)),
-        "secs" => Ok(Duration::from_secs(helper.value)),
-        "s" => Ok(Duration::from_secs(helper.value)),
-        "milliseconds" => Ok(Duration::from_millis(helper.value)),
-        "millis" => Ok(Duration::from_millis(helper.value)),
-        "ms" => Ok(Duration::from_millis(helper.value)),
-        "microseconds" => Ok(Duration::from_micros(helper.value)),
-        "micros" => Ok(Duration::from_micros(helper.value)),
-        "us" => Ok(Duration::from_micros(helper.value)),
-        "nanoseconds" => Ok(Duration::from_nanos(helper.value)),
-        "nanos" => Ok(Duration::from_nanos(helper.value)),
-        "ns" => Ok(Duration::from_nanos(helper.value)),
-        // ... add other units as needed
-        _ => Err(serde::de::Error::custom("Unsupported duration unit")),
-    }
-}
